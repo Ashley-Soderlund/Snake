7c3d64f87137605d6e89d425b7a770ff6de038f3
Ashley Soderlund <69334327+Ashley-Soderlund@users.noreply.github.com>
2022-04-28 10:55:19 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace SnakeGame {     
    //Driver Class
@@ -13,13 +13,26 @@ namespace SnakeGame {
       // }
 
       static void Main (string[] args){
-         //CreateBoard();
-         Snake boi = new Snake(2);
-         Fruit yum = new Fruit();
+         Console.WriteLine("Do you wish to play Snake? Type 'yes' to PLAY or 'no' to QUIT");
+         string userPlay = Console.ReadLine();
+         while (userPlay.Length == 3){
+            //CreateBoard();
+            Snake boi = new Snake(2);
+            Fruit yum = new Fruit();
+            
+            //Start Game
+            //while lose() == false
+               //get user input and update snake
+               //check if wine() == true
+            //show score?
 
-         boi.print(boi);
-         Console.WriteLine("Hello World!");
-         Console.ReadKey();
+            boi.print(boi);
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            Console.WriteLine("Do you wish to play Snake? Type 'yes' to PLAY or 'no' to QUIT");
+            userPlay = Console.ReadLine();
+         }
       }
    }
 }
