326c1bcd37dd83b3fb37e2ceb8147d8f1674e711
Ashley Soderlund <69334327+Ashley-Soderlund@users.noreply.github.com>
2022-05-02 16:15:48 -0500

    Update Snake.cs

--- Snake.cs
+++ Snake.cs
@@ -5,19 +5,39 @@ namespace SnakeGame {
       //Snake class creation
 
       private int snakeLegnth;
+      private int snakeYaxis;
+      private int snakeXaxis;
 
       public Snake(int _snakeLegnth){
           snakeLegnth = _snakeLegnth;
+          snakeXaxis = 10;
+          snakeXaxis = 10;
       }
 
       //accessors
       public int Size
       {
-         get
-         {return snakeLegnth;}
-         set
-         {snakeLegnth = value;}
+         get{return snakeLegnth;}
+         set{snakeLegnth = value;}
       }
+
+      public int XaxisSnake
+      {
+         get{return snakeXaxis;}
+         set{snakeXaxis = value;}
+      }
+
+      public int YaxisSnake
+      {
+         get{return snakeYaxis;}
+         set{snakeYaxis = value;}
+      }
+
+      //UpdateSnake()
+      //SnakeGrow()
+      //Movement()?
+      
+
 
       public void print(Snake snake){
           Console.WriteLine("Snakes Legnth is " + snake.Size);
