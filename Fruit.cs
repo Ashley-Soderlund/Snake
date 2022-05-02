5a11ddea94255ffd73f5eeb6f82810648a407c0c
Ashley Soderlund <69334327+Ashley-Soderlund@users.noreply.github.com>
2022-05-02 16:14:57 -0500

    Update Fruit.cs

--- Fruit.cs
+++ Fruit.cs
@@ -1,11 +1,44 @@
 
 namespace SnakeGame {
    public class Fruit{
-       // char FruitCharacter
-       // FruitLocation
-       // RandomLocation()
-       // NewFruit()
-       // DisplayFruit()
+      private int fruitYaxis;
+      private int fruitXaxis;
+
+
+      public Fruit(){
+         fruitXaxis = 5;
+         fruitYaxis = 5;
+      }
+
+      //Setters and Getters for Fruit Values
+      public int XaxisFruit
+      {
+         get{return fruitXaxis;}
+         set{fruitXaxis = value;}
+      }
+
+      public int YaxisFruit
+      {
+         get{return fruitYaxis;}
+         set{fruitYaxis = value;}
+      }
+
+      //Get a Random number from 1 to 18
+      public int RandomLocation(){
+         Random rnd = new Random();
+         int locationvalue = rnd.Next(1, 19);
+         return locationvalue;
+      }
+
+      //Updates Fruit's Location Values with Random Numbers Function
+      public void UpdateFruit(Fruit yum){
+
+         int randomLocationX = RandomLocation();
+         int randomLocationY = RandomLocation();
+
+         yum.XaxisFruit = randomLocationX;
+         yum.YaxisFruit = randomLocationY;
+      }
    }
 
 }
